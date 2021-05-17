4d98caf24a329f8ae5e82394bfe9b7ae482d525d
DevDray <debasisddray3@gmail.com>
2021-05-17 22:05:28 +0530

    End of Section 5: Client login and register

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,34 @@
+import { AccountService } from './../_services/account.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {  
+
+@Output()
+cancelRegister = new EventEmitter();
+
+  model: any = {};
+
+  constructor(private accountservice: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    this.accountservice.register(this.model).subscribe(response => {
+      console.log(response);
+      this.cancel();
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+
+}
